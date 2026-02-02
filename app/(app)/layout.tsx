511e9e133d6a2108e099c6a30bb17587385bb15c
Gianfranco Grenier <gianfranco.grenier@avattar.com>
2026-02-02 16:34:21 -0300

    updated logo redirect url home depot

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -15,7 +15,7 @@ export default async function Layout({ children }: LayoutProps) {
         <a
           target="_blank"
           rel="noopener noreferrer"
-          href="https://livekit.io"
+          href="https://www.homedepot.com.mx"
           className="scale-100 transition-transform duration-300 hover:scale-110"
         >
           {/* eslint-disable-next-line @next/next/no-img-element */}
